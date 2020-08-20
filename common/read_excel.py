99ac2e5851b61786de6f99ef0f665dbc5a4b2707
songjhm <songjhm@yonyou.com>
2020-08-20 13:42:02 +0800

    补充

--- common/read_excel.py
+++ common/read_excel.py
@@ -7,7 +7,7 @@ import xlrd
 
 class read_excel():
 
-    '''读取Excel数据，并返回字典，用于测试框架'''
+    '''读取Excel数据，并返回列表，用于测试框架'''
 
     def __init__(self, file_name, sheet_name = 'sheet1'):
         self.data = xlrd.open_workbook(file_name)
@@ -18,5 +18,14 @@ class read_excel():
 
     def read_data(self):
         if self.nrows > 1:
-            pass
-
+            '''获取第一行数据作为key，返回列表格式'''
+            keys = self.table.row_values(0)
+            list_apidata = []
+            for col in range(1, self.nrows):
+                values = self.table.row_values(col)
+                api_dict = dict(zip(keys , values))
+                list_apidata.append(api_dict)
+            return list_apidata
+        else:
+            print('该文件内容为空，请检查文件是否正确！')
+            return None
